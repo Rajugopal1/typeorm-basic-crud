7489cb4ec2f3bf2b9b7316e90ebca0d3211e2c90
Rajugopal1 <rajugopalsinghh@gmail.com>
2022-09-06 16:14:39 +0530

    crud created

---
+++ src/routes/postRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import PostController from "../controllers/Posts";
+
+const router = Router();
+
+router.post("/post", PostController.postPost);
+router.get("/post", PostController.getPost);
+router.get("/post/:id", PostController.getOnePost);
+router.patch("/post/:id", PostController.updatePost);
+router.delete("/post/:id", PostController.deletePost);
+export default router;
